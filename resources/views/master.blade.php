04867b78c79f244f99e89f9787005a62922820c4
Gunharth Randolf <hello@gunharth.io>
2016-02-19 17:25:40 +0100

    Added laravel auth

---
+++ resources/views/master.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>wongaApp</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">ReelsApp</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          @if (Auth::check())
+          <li><a href="{{ url('/home') }}">Dashboard</a></li>
+          <li><a href="/clips">Clips</a></li>
+          <li><a href="/reels">Reels</a></li>
+          <li><a href="/directors">Directors</a></li>
+          @endif
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <!--<li><a href="{{ url('/register') }}">Register</a></li>-->
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
+
+	<div class="container">
+		@include('partials.errors')
+    @if(Session::has('flash_message'))
+          <div id="flashMessage">
+          <div class="alert alert-success">
+              {{ Session::get('flash_message') }}
+          </div>
+          </div>
+      @endif
+    @yield('content')
+	</div>
+	
+<script src="/js/app.js"></script>
+	
+</body>
+</html>
